20b44dad3bed500234789594bdb7bcdc9b3b97c7
snufkinly <snufkinly@users.noreply.github.com>
2015-09-27 16:25:02 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,61 @@
 ## functions do
 
 ## Write a short comment describing this function
+## function that can make a matrix, get the matrix, make an inverse and put it in the cache
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function( m = matrix()) {
+    #set cache initially to NULL
+    cache = NULL
+    
+    #make matrix
+    setmatrix <- function(y) {
+        m <<- y
+        cache <<- NULL
+    }
+    
+    #get the matrix  
+    getmatrix <- function() m
+    
+    #make inverse of matrix and cache it
+    setinversematrix <- solve(m)
+    cache <<- setinversematrix
+    
+    #get inverse of matrix from cache
+    getinverse <- function() cache
+  
+    #put created functions into working environment  
+    list(setmatrix = setmatrix, getmatrix=getmatrix,
+         setinversematrix=setinversematrix,
+         getinverse=getinverse)
+  
 }
 
+## a function that can check if there is a inverse matrix in the cache and if so use it, if not
+## make a new inverse matrix and put in the cache
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(m, ...) {
+  cache <- m$getinverse()
+  
+  if (!is.null(cache)) {
+    print("getting matrix from cache")
+    return(cache)
+    
+  }
+  newmatrix <- m$getmatrix()    
+  
+  if (is.null(cache)) {
+    print("calculating inverse")
+    
+    nm_inverse <- solve(newmatrix)
+    cache <- nm_inverse
+    
+  put_in = {
+    
+    cache <<- nm_inverse
+  }
+    
+    return(cache)
+  }
+  
+} 
+  
